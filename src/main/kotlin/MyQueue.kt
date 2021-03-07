7b095ca2f0f125b8bde8f8b13d17298311ccc941
Ilia Rodionov <ilia.rodionov20021801@gmail.com>
2021-03-08 01:45:25 +0300

    Реализовал свою очередь через ноды

--- src/main/kotlin/MyQueue.kt
+++ src/main/kotlin/MyQueue.kt
@@ -1,4 +1,5 @@
 import java.util.*
+import kotlin.NoSuchElementException
 
 fun <T> queueOf(vararg elements: T): Queue<T> = elements.asQueue()
 fun <T> queueOf(): Queue<T> = MyQueue()
@@ -10,23 +11,51 @@ fun <T> Array<out T>.asQueue(): Queue<T> {
 }
 
 class MyQueue<T> : Queue<T> {
-    private val _queue: MutableList<T> = LinkedList<T>()
+    private var first: Node<T>? = null
+    private var last: Node<T>? = null
 
     inner class QueueIterator : Iterator<T> {
-        private var cursor: Int = 0
+        private var cursor = first
 
-        override fun hasNext(): Boolean = cursor < _queue.size
+        override fun hasNext(): Boolean = cursor != null
 
-        override fun next(): T = _queue[cursor++]
+        override fun next(): T {
+            val current = cursor ?: throw NoSuchElementException()
+            val element = current.item ?: throw NoSuchElementException()
+            cursor = current.next
+            return element
+        }
     }
 
     override fun enqueue(element: T) {
-        _queue.add(element)
+        val f = last
+        val newNode = Node(element, null)
+        last = newNode
+
+        if (f == null)
+            first = newNode
+        else
+            f.next = newNode
     }
 
-    override fun dequeue(): T? = if (_queue.isEmpty()) null else _queue.removeFirst()
+    override fun dequeue(): T? {
+        val f = first ?: return null
 
-    override fun peek(): T? = if (_queue.isEmpty()) null else _queue.first()
+        val element = f.item
+        first = f.next
+
+        f.next = null
+        f.item = null
+
+        if (first == null)
+            last = null
+
+        return element
+    }
+
+    override fun peek(): T? = first?.item
 
     override fun iterator(): Iterator<T> = QueueIterator()
+
+    private data class Node<E>(var item: E?, var next: Node<E>?)
 }
