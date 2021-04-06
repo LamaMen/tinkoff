11c3ba98eef8633548d330c074bda000e926dba3
Rodionov Ilia <ilia.rodionov20021801@gmail.com>
2021-04-06 16:46:19 +0300

    Испраивл тест ошибок

--- src/test/kotlin/ExtensionsTest.kt
+++ src/test/kotlin/ExtensionsTest.kt
@@ -1,20 +1,17 @@
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertAll
 import org.junit.jupiter.api.assertThrows
 
 class ExtensionsTest {
 
     @Test
-    fun `test extension function in the correct case`() {
-        assertAll(
-            { assertFalse("Hello".isEvenLength()) },
-            { assertTrue("Hi".isEvenLength()) },
-            { assertFalse("123".isEvenLength()) },
-            { assertTrue("12".isEvenLength()) },
-            { assertTrue("privet".isEvenLength()) },
-            { assertFalse("bye".isEvenLength()) },
-        )
+    fun `test extension function on even string`() {
+        assertTrue("Hi".isEvenLength())
+    }
+
+    @Test
+    fun `test extension function on not even string`() {
+        assertFalse("Hello".isEvenLength())
     }
 
     @Test
