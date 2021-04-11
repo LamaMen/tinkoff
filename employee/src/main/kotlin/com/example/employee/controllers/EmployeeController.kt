4ac8e2eb02a88529942b67b5bd895ddb77a8570e
Ilia Rodionov <ilia.rodionov20021801@gmail.com>
2021-04-11 16:48:05 +0300

    Добавил описание API

--- employee/src/main/kotlin/com/example/employee/controllers/EmployeeController.kt
+++ employee/src/main/kotlin/com/example/employee/controllers/EmployeeController.kt
@@ -2,41 +2,60 @@ package com.example.employee.controllers
 
 import com.example.employee.dao.DAO
 import com.example.employee.models.Employee
-import org.springframework.http.HttpStatus
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.server.ResponseStatusException
 
 @RestController()
 @RequestMapping("/employee")
 class EmployeeController(val employeeDAO: DAO<Employee>) {
 
     @GetMapping()
+    @Operation(summary = "Возращает список всех сотрудников")
+    @ApiResponse(responseCode = "200", description = "Все хорошо")
     fun getAllEmployees() = employeeDAO.getAll()
 
     @GetMapping("/{id}")
-    fun getEmployeeById(@PathVariable id: Int): Employee {
+    @Operation(summary = "Возращает сотрудника по id")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "Все хорошо"),
+            ApiResponse(responseCode = "404", description = "Сотрудника с таким id нет")
+        ]
+    )
+    fun getEmployeeById(@PathVariable @Parameter(name = "Идентифтор сотрудника") id: Int): Employee {
         return employeeDAO.getById(id)
     }
 
     @PostMapping
-    fun addEmployee(@RequestBody employee: Employee): Employee {
+    @Operation(summary = "Добавляет нового сотрудника")
+    @ApiResponse(responseCode = "200", description = "Сотрудник добавлен успешно")
+    fun addEmployee(@RequestBody @Parameter(name = "Сотрудник, который будет добавлен") employee: Employee): Employee {
         employeeDAO.add(employee)
         return employee
     }
 
     @PutMapping("/{id}")
-    fun updateEmployee(@PathVariable id: Int, @RequestBody employee: Employee): Employee {
+    @Operation(summary = "Обновляет данные сотрудника по id")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "Данные успешно обновлены"),
+            ApiResponse(responseCode = "404", description = "Сотрудника с таким id нет")
+        ]
+    )
+    fun updateEmployee(
+        @PathVariable @Parameter(name = "Идентифтор сотрудника, данные которого надо обновить") id: Int,
+        @RequestBody @Parameter(name = "Данные которые будут установлены") employee: Employee
+    ): Employee {
         return employeeDAO.update(id, employee)
     }
 
     @DeleteMapping("{id}")
-    fun deleteEmployee(@PathVariable id: Int) {
+    @Operation(summary = "Удаляет сотрудника по его id")
+    @ApiResponse(responseCode = "200", description = "Сотрудник успешно удалён")
+    fun deleteEmployee(@PathVariable @Parameter(name = "Идентифтор сотрудника, данные которого будут удалены") id: Int) {
         employeeDAO.delete(id)
     }
-
-    @ExceptionHandler(NoSuchElementException::class)
-    fun handleException() {
-        throw ResponseStatusException(HttpStatus.NOT_FOUND)
-    }
-
 }
--- employee/src/main/kotlin/com/example/employee/models/EmployeeWithSalary.kt
+++ employee/src/main/kotlin/com/example/employee/models/EmployeeWithSalary.kt
@@ -1,9 +1,12 @@
 package com.example.employee.models
 
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "Сотрудник")
 class EmployeeWithSalary(
-    id: Int,
-    name: String,
-    age: Int,
-    position: String,
-    val salaryAmount: Long
+    @Schema(description = "Идентификатор сотрудника") id: Int,
+    @Schema(description = "Имя сотрудника") name: String,
+    @Schema(description = "Возраст сотрудника") age: Int,
+    @Schema(description = "Должность сотрудника") position: String,
+    @Schema(description = "Зарплата сотрудника") val salaryAmount: Long
 ) : Employee(id, name, age, position)
