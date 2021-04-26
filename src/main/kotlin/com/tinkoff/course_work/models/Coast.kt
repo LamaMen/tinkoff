dd4c7992d6445968b9600047250dfb36e7e0739b
Ilia Rodionov <ilia.rodionov20021801@gmail.com>
2021-04-27 00:30:35 +0300

    MF-19 Поправил сериализацию времени

--- src/main/kotlin/com/tinkoff/course_work/models/Coast.kt
+++ src/main/kotlin/com/tinkoff/course_work/models/Coast.kt
@@ -1,11 +1,13 @@
 package com.tinkoff.course_work.models
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.time.LocalDateTime
 
 data class Coast(
     val id: Int?,
     val title: String,
     val amount: Long,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd MMMM yyyy HH:mm")
     val date: LocalDateTime?
 ) {
     companion object {
